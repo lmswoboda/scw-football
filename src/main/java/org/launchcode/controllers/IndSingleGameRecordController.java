8bae51b9c40952d5d5c2ccf5f7bcdec8fb7fb65b
Lynn Strauss <lmswoboda@fandbfin.com>
2017-08-26 18:27:43 -0500

    Finished IndSignleGameRecord

--- src/main/java/org/launchcode/controllers/IndSingleGameRecordController.java
+++ src/main/java/org/launchcode/controllers/IndSingleGameRecordController.java
@@ -4,18 +4,76 @@ import org.launchcode.models.IndSingleGameRecord;
 import org.launchcode.models.data.IndSingleGameRecordDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
  * Created by lynnstrauss on 8/25/17.
  */
 
 @Controller
-@RequestMapping("IndSingleGameRecord")
+@RequestMapping("indSingleGameRecord")
 public class IndSingleGameRecordController {
 
     @Autowired
     private IndSingleGameRecordDao indSingleGameRecordDao;
 
 
+    //    Request path: /indSingleGameRecord
+    @RequestMapping(value = "")
+    public String indSingleGameRecord(Model model){
+        model.addAttribute("indSingleGameRecords", indSingleGameRecordDao.findAll());
+        model.addAttribute("title", "Individual Single Game Records");
+        return "indSingleGameRecord/index";
+    }
+
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddIndSingleGameRecordForm(Model model) {
+        model.addAttribute("title", "Add Individual Single Game Record");
+        model.addAttribute(new IndSingleGameRecord());
+        return "indSingleGameRecord/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddIndSingleGameRecordForm(@ModelAttribute @Valid IndSingleGameRecord newIndSingleGameRecord,
+                                             Errors errors,
+                                             Model model) {
+
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Individual Single Game Record");
+            return "indSingleGameRecord/add";
+        }
+
+        indSingleGameRecordDao.save(newIndSingleGameRecord);
+
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveIndSingleGameRecordForm(Model model) {
+        model.addAttribute("indSingleGameRecords", indSingleGameRecordDao.findAll());
+        model.addAttribute("title", "Remove Individual Single Game Record");
+        return "indSingleGameRecord/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveIndSingleGameRecordForm(@RequestParam int[] indSingleGameRecordIds) {
+
+        for (int indSingleGameRecordId : indSingleGameRecordIds) {
+            indSingleGameRecordDao.delete(indSingleGameRecordId);
+        }
+
+        return "redirect:";
+    }
+
+
 }
+
